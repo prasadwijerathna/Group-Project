337ae6dbc0c12dcdc574df01b01c08571670ee60
Prasad Wijerathna <prasadwijerathna03080@gmail.com>
2025-06-06 21:37:39 +0530

    connect php

---
+++ public/admin.php
@@ -0,0 +1,22 @@
+<?php
+// Database connection
+$servername = "127.0.0.1";
+$username = "root";
+$password = "mariadb";
+$dbname = "homeheaven";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch items from the database
+$sql1 = "SELECT * FROM items";
+$result = $conn->query($sql1);
+
+$sql2 = "SELECT * FROM customer";
+$resultuser = $conn->query($sql2);
+
+
