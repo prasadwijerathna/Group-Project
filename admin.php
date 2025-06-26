821fcc270431e49c39fc4f2d34fc91af91e4753d
Prasad Wijerathna <139612338+prasadwijerathna@users.noreply.github.com>
2025-06-26 17:52:23 +0530

    Update admin.php
    database connection

--- admin.php
+++ admin.php
@@ -1,22 +1,16 @@
 <?php
-// Database connection
-$servername = "127.0.0.1";
-$username = "root";
-$password = "mariadb";
-$dbname = "pet";
+$servername = "localhost";
+$username = "root";      // change if needed
+$password = "";          // change if needed
+$dbname = "petpulse";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
-
-// Fetch items from the database
-$sql1 = "SELECT * FROM items";
-$result = $conn->query($sql1);
-
-$sql2 = "SELECT * FROM customer";
-$resultuser = $conn->query($sql2);
+?>
 
 
