93c969e048e7cd1f20d8db5a8fc52eaf166c569b
GMPBhagya <139644464+GMPBhagya@users.noreply.github.com>
2025-06-30 11:08:54 +0000

    add style

--- trainfrom.php
+++ trainfrom.php
@@ -1,5 +1,10 @@
 <section class="form-section">
   <h2>Register for a Training Session</h2>
   <style>
-    
+    .form-section {
+  background-color: #fff8f0;
+  padding: 30px;
+  margin-top: 40px;
+  }
+
   </style>
