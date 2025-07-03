719a1c5cbfa91c99de1fd8b0b451e23d22cb5455
GMPBhagya <139644464+GMPBhagya@users.noreply.github.com>
2025-07-03 07:20:19 +0000

    add style

--- trainfrom.php
+++ trainfrom.php
@@ -7,6 +7,14 @@
   margin-top: 40px;
   border-radius: 12px;
   box-shadow: 0 4px 10px rgba(0,0,0,0.1);
+    max-width: 600px;
+  margin-left: auto;
+  margin-right: auto;
   }
-
+.form-section h2 {
+  text-align: center;
+  color: #ff6f61;
+  margin-bottom: 20px;
+}
   </style>
+  
