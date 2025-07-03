0e781d20105a27831d4d31796698a6e3c4019934
GMPBhagya <139644464+GMPBhagya@users.noreply.github.com>
2025-07-03 07:21:41 +0000

    create form

--- trainfrom.php
+++ trainfrom.php
@@ -18,3 +18,25 @@
 }
   </style>
   
+
+
+  <form id="trainingForm" method="POST" action="" >
+    <div class="form-group">
+        <label for="username">Name</label>
+      <textarea id="owner" name="owner_name" rows="1" placeholder="Owner name"></textarea>
+
+    </div>
+
+    <div class="form-group">
+   
+
+      <label for="message">Message (optional)</label>
+      <textarea id="message" name="message" rows="4" placeholder="Anything you'd like us to know?"></textarea>
+
+      <label for="telephone no">Mobile No</label>
+      <textarea id="Mobile No" name="MobileNo" rows="1" placeholder="MobileNo"></textarea>
+        
+  
+    <button type="submit" name="submit">Submit</button>
+  </form>
+</section>
